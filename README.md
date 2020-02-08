72a76c138407166d9ebaa44b4347054d5545cfad
Vitan <me@vitan.me>
2020-02-08 21:19:44 +0800

    📦 NEW: New Version

---
+++ README.md
@@ -0,0 +1,7 @@
+<p align="center" class="has-mb-6">
+<img class="not-gallery-item" height="48" src="https://vitan.me/images/vitan.png">
+<br>
+<h2 align="center">WNMP</h2>
+<p align="center">Windows + Nginx(1.14) + PHP7.3 + MySql5.7</p>
+</p>
+
---
+++ Restart.bat
@@ -0,0 +1,25 @@
+@echo off
+echo Stopping Nginx...
+taskkill /F /IM nginx.exe > nul
+echo Stopping PHP FastCGI...
+taskkill /F /IM php-cgi.exe > nul
+echo Stopping Mysql...
+taskkill /F /IM mysqld.exe > nul
+
+set base_path=%cd%
+set nginx_path=%base_path%\nginx
+set php7_path=%base_path%\php\php-7.2.25
+set mysql_path=%base_path%\mysql
+
+echo Starting PHP7 FastCGI...
+RunHiddenConsole %php7_path%\php-cgi.exe -b 127.0.0.1:9000 -c %php7_path%\php.ini
+
+echo Starting Nginx...
+RunHiddenConsole %nginx_path%\nginx.exe -c %nginx_path%\conf\nginx.conf
+
+echo Starting MySql...
+RunHiddenConsole %mysql_path%\bin\mysqld --defaults-file=%mysql_path%\my.ini --port=3306
+echo please open http://127.0.0.1 ...
+ping -n 3 127.0.0.1 > nul
+start "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Google Chrome.lnk" "http://127.0.0.1"
+exit
