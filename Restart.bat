fe044659c6aabc852e43338c8260e2ba0edf5163
Vitan <vitan.me@gmail.com>
2020-03-31 22:05:45 +0800

    🐛 FIX: Batch Scripts Bug

--- Restart.bat
+++ Restart.bat
@@ -15,14 +15,14 @@ set php7_path=%base_path%\php\php-7.2.25
 set mysql_path=%base_path%\mysql
 
 echo Starting PHP7 FastCGI...
-%php7_path%\php-cgi.exe -b 127.0.0.1:9000 -c %php7_path%\php.ini
+RunHiddenConsole %php7_path%\php-cgi.exe -b 127.0.0.1:9000 -c %php7_path%\php.ini
 
 echo Starting Nginx...
-%nginx_path%\nginx.exe -c %nginx_path%\conf\nginx.conf
+RunHiddenConsole %nginx_path%\nginx.exe -c %nginx_path%\conf\nginx.conf
 
 echo Starting MySql...
-%mysql_path%\bin\mysqld --defaults-file=%mysql_path%\my.ini --port=3306
-echo Openning http://127.0.0.1 ...
+RunHiddenConsole %mysql_path%\bin\mysqld --defaults-file=%mysql_path%\my.ini --port=3306
+echo please open http://127.0.0.1 ...
 ping -n 3 127.0.0.1 > nul
-start chrome  "http://dev.vitan.me"
+start chrome  "http://127.0.0.1"
 exit
--- Start.bat
+++ Start.bat
@@ -5,21 +5,21 @@ mode con cols=50 lines=20&color 0a
 set base_path=%cd%
 set nginx_path=%base_path%\nginx
 set php7_path=%base_path%\php\php-7.2.25
-set mysql_path=%base_path%\mysql
+set mysql_path=%base_path%\mySql
 
 ::echo Starting PHP5 FastCGI...
 ::RunHiddenConsole %php5_path%\php5-cgi.exe -b 127.0.0.1:9001 -c %php5_path%\php.ini
 
 echo Starting PHP7 FastCGI...
-%php7_path%\php-cgi.exe -b 127.0.0.1:9000 -c %php7_path%\php.ini
+RunHiddenConsole %php7_path%\php-cgi.exe -b 127.0.0.1:9000 -c %php7_path%\php.ini
 
 echo Starting Nginx...
-%nginx_path%\nginx.exe -c %nginx_path%\conf\nginx.conf
+RunHiddenConsole %nginx_path%\nginx.exe -c %nginx_path%\conf\nginx.conf
 
 echo Starting MySql...
-%mysql_path%\bin\mysqld --defaults-file=%mysql_path%\my.ini --port=3306
+RunHiddenConsole %mysql_path%\bin\mysqld --defaults-file=%mysql_path%\my.ini --port=3306
 
-echo Openning http://127.0.0.1 ...
+echo please open http://127.0.0.1 ...
 ping -n 3 127.0.0.1 > nul
-start chrome  "dev.vitan.me"
-pause
+start chrome  "127.0.0.1"
+exit
